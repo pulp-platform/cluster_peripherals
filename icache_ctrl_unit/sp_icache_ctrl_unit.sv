0085a89d25d971d45121e0eee5f7137af785ba9d
Andreas Kurth <akurth@iis.ee.ethz.ch>
2019-03-08 08:28:27 +0100

    sp_icache_ctrl_unit: Define type of parameters

--- icache_ctrl_unit/sp_icache_ctrl_unit.sv
+++ icache_ctrl_unit/sp_icache_ctrl_unit.sv
@@ -50,10 +50,10 @@
 
 module sp_icache_ctrl_unit
 #(
-    parameter  NB_CACHE_BANKS = 4,
-    parameter  NB_CORES       = 4,
-    parameter  ID_WIDTH       = 5,
-    parameter  OFFSET         = 4
+    parameter int NB_CACHE_BANKS  = 4,
+    parameter int NB_CORES        = 4,
+    parameter int ID_WIDTH        = 5,
+    parameter int OFFSET          = 4
 )
 (
     input logic                                 clk_i,
